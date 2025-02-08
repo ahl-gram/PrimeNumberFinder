5da30191c09ed4a8f3d57d56806ae55267c24414
alexanderhlee <alexanderhlee@gmail.com>
2025-02-08 13:58:25 -0500

    Initial commit with Xcode project setup

---
+++ PrimeFinderApp/PrimeFinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeFinderAppApp.swift
+//  PrimeFinderApp
+//
+//  Created by Alexander Lee on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct PrimeFinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PrimeFinderAppTests/PrimeFinderAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeFinderAppTests.swift
+//  PrimeFinderAppTests
+//
+//  Created by Alexander Lee on 2/8/25.
+//
+
+import Testing
+@testable import PrimeFinderApp
+
+struct PrimeFinderAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
